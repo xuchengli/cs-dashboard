0c4d281a9f1df58e73f263374f420488fbb60626
lixucheng <lixucheng@aliyun.com>
2017-09-14 17:48:09 +0800

    update login component using uikit css

--- web/router/components/login.vue
+++ web/router/components/login.vue
@@ -1,3 +1,26 @@
 <template>
-    <div>Login</div>
+    <div class="uk-card uk-card-default uk-width-large uk-align-center uk-margin-auto-vertical">
+        <div class="uk-card-header">
+            <h3 class="uk-card-title">Login</h3>
+        </div>
+        <div class="uk-card-body">
+            <form>
+                <div class="uk-margin">
+                    <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: user"></span>
+                        <input class="uk-input" type="text">
+                    </div>
+                </div>
+                <div class="uk-margin uk-margin-remove-bottom">
+                    <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                        <input class="uk-input" type="text">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="uk-card-footer">
+            <button class="uk-button uk-button-primary uk-align-right">Login</button>
+        </div>
+    </div>
 </template>
