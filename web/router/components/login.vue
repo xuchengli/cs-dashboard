a27a67f4f5ce92be6af15f2e62714fe7c77dfb5d
lixucheng <lixucheng@aliyun.com>
2017-09-15 17:40:40 +0800

    update login and register components using uikit css

--- web/router/components/login.vue
+++ web/router/components/login.vue
@@ -8,19 +8,56 @@
                 <div class="uk-margin">
                     <div class="uk-inline uk-width-1-1">
                         <span class="uk-form-icon" uk-icon="icon: user"></span>
-                        <input class="uk-input" type="text">
+                        <input class="uk-input" type="text" placeholder="User ID"
+                            :class="userIdClass" v-model="userId"
+                            @focus="userIdActive = true" @blur="userIdActive = false">
+                    </div>
+                </div>
+                <div class="uk-margin">
+                    <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                        <input class="uk-input" type="password" placeholder="Password"
+                            :class="passwordClass" v-model="password"
+                            @focus="passwordActive = true" @blur="passwordActive = false">
                     </div>
                 </div>
                 <div class="uk-margin uk-margin-remove-bottom">
-                    <div class="uk-inline uk-width-1-1">
-                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
-                        <input class="uk-input" type="text">
-                    </div>
+                    <label>
+                        <input class="uk-checkbox" type="checkbox" v-model="checked">
+                        Remember me
+                    </label>
                 </div>
             </form>
         </div>
         <div class="uk-card-footer">
-            <button class="uk-button uk-button-primary uk-align-right">Login</button>
+            <router-link to="/register" class="uk-button uk-button-default">Register</router-link>
+            <button class="uk-button uk-button-primary uk-align-right" @click="login">Login</button>
         </div>
     </div>
 </template>
+<script>
+    export default {
+        data() {
+            return {
+                userId: "",
+                password: "",
+                checked: false,
+                userIdActive: true,
+                passwordActive: true
+            }
+        },
+        computed: {
+            userIdClass() {
+                return this.userIdActive ? "" : this.userId === "" ? "uk-form-danger" : "";
+            },
+            passwordClass() {
+                return this.passwordActive ? "" : this.password === "" ? "uk-form-danger" : "";
+            }
+        },
+        methods: {
+            login() {
+                console.log(this.userId, this.password, this.checked);
+            }
+        }
+    }
+</script>
--- web/router/components/register.vue
+++ web/router/components/register.vue
@@ -1,3 +1,35 @@
 <template>
-    <div>Register</div>
+    <div class="uk-card uk-card-default uk-width-large uk-align-center uk-margin-auto-vertical">
+        <div class="uk-card-header">
+            <h3 class="uk-card-title">
+                <router-link to="/" uk-icon="icon: arrow-left; ratio: 1.5"></router-link>
+                Register
+            </h3>
+        </div>
+        <div class="uk-card-body">
+            <form>
+                <div class="uk-margin">
+                    <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: user"></span>
+                        <input class="uk-input" type="text" placeholder="User ID">
+                    </div>
+                </div>
+                <div class="uk-margin">
+                    <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                        <input class="uk-input" type="password" placeholder="Password">
+                    </div>
+                </div>
+                <div class="uk-margin uk-margin-remove-bottom">
+                    <div class="uk-inline uk-width-1-1">
+                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
+                        <input class="uk-input" type="password" placeholder="Retype password">
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="uk-card-footer">
+            <button class="uk-button uk-button-primary uk-align-right">Register</button>
+        </div>
+    </div>
 </template>
