3be738174cf364218cc4d2695a9eeedc56bbd9e5
lixucheng <lixucheng@aliyun.com>
2017-09-25 14:00:27 +0800

    add register router

--- web/router/components/register.vue
+++ web/router/components/register.vue
@@ -9,27 +9,97 @@
         <div class="uk-card-body">
             <form>
                 <div class="uk-margin">
-                    <div class="uk-inline uk-width-1-1">
-                        <span class="uk-form-icon" uk-icon="icon: user"></span>
-                        <input class="uk-input" type="text" placeholder="User ID">
-                    </div>
+                    <verifiable-input class="uk-width-1-1"
+                        icon="user" placeholder="User ID" v-model="userId.value"
+                        :status.sync="userId.status" :message.sync="userId.message"
+                        @blur="validateUserId">
+                    </verifiable-input>
                 </div>
                 <div class="uk-margin">
-                    <div class="uk-inline uk-width-1-1">
-                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
-                        <input class="uk-input" type="password" placeholder="Password">
-                    </div>
+                    <verifiable-input class="uk-width-1-1"
+                        icon="lock" type="password" placeholder="Password"
+                        v-model="password.value"
+                        :status.sync="password.status" :message.sync="password.message"
+                        @blur="validatePassword">
+                    </verifiable-input>
                 </div>
                 <div class="uk-margin uk-margin-remove-bottom">
-                    <div class="uk-inline uk-width-1-1">
-                        <span class="uk-form-icon" uk-icon="icon: lock"></span>
-                        <input class="uk-input" type="password" placeholder="Retype password">
-                    </div>
+                    <verifiable-input class="uk-width-1-1"
+                        icon="lock" type="password" placeholder="Retype password"
+                        v-model="retypePassword.value"
+                        :status.sync="retypePassword.status" :message.sync="retypePassword.message"
+                        @blur="validateRetypePassword">
+                    </verifiable-input>
                 </div>
             </form>
         </div>
         <div class="uk-card-footer">
-            <button class="uk-button uk-button-primary uk-align-right">Register</button>
+            <button class="uk-button uk-button-primary uk-align-right" @click="register">
+                Register
+            </button>
         </div>
     </div>
 </template>
+<script>
+    import VerifiableInput from "../../components/verifiable-input.vue";
+
+    export default {
+        data() {
+            return {
+                userId: {
+                    value: "",
+                    status: "normal",
+                    message: ""
+                },
+                password: {
+                    value: "",
+                    status: "normal",
+                    message: ""
+                },
+                retypePassword: {
+                    value: "",
+                    status: "normal",
+                    message: ""
+                }
+            }
+        },
+        methods: {
+            validateUserId() {
+                if (this.userId.value === "") {
+                    this.userId.status = "warning";
+                    this.userId.message = "Please input use id.";
+                    return false;
+                }
+                return true;
+            },
+            validatePassword() {
+                if (this.password.value === "") {
+                    this.password.status = "warning";
+                    this.password.message = "Please input password.";
+                    return false;
+                }
+                return true;
+            },
+            validateRetypePassword() {
+                if (this.retypePassword.value === "") {
+                    this.retypePassword.status = "warning";
+                    this.retypePassword.message = "Please retype password.";
+                    return false;
+                } else if (this.retypePassword.value != this.password.value) {
+                    this.retypePassword.status = "warning";
+                    this.retypePassword.message = "Please confirm password.";
+                    return false;
+                }
+                return true;
+            },
+            register() {
+                if (this.validateUserId() && this.validatePassword() && this.validateRetypePassword()) {
+                    console.log(this.userId.value, this.password.value);
+                }
+            }
+        },
+        components: {
+            VerifiableInput
+        }
+    }
+</script>
