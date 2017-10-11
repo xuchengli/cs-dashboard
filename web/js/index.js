9d81b122b394e9484f6d81d94e9f520e1271aef5
lixucheng <lixucheng@aliyun.com>
2017-10-11 16:07:04 +0800

    init default lang by detect browser language and cache lang using vue-cookie

--- web/js/index.js
+++ web/js/index.js
@@ -1,6 +1,7 @@
 import "uikit/dist/css/uikit.css";
 import Vue from "vue";
 import VueI18n from "vue-i18n";
+import VueCookie from "vue-cookie";
 import UIkit from "uikit";
 import Icons from "uikit/dist/js/uikit-icons";
 import router from "../router";
@@ -8,31 +9,42 @@ import LocaleSelect from "../components/locale-select.vue";
 
 UIkit.use(Icons);
 Vue.use(VueI18n);
+Vue.use(VueCookie);
 Vue.component("locale-select", LocaleSelect);
 
-new Vue({
-    router,
-    data: {
-        locale: "zh-CN"
-    },
-    i18n: new VueI18n({
-        locale: "zh-CN",
-        silentTranslationWarn: true,
-        messages: {
-            "en": {
-                "global": {
-                    "error": {
-                        "500": "Internal Server Error."
-                    }
+let lang = Vue.cookie.get("locale") || navigator.language || navigator.userLanguage;
+if (lang != "en" && lang != "zh-CN") lang = "en";
+
+const i18n = new VueI18n({
+    locale: lang,
+    silentTranslationWarn: true,
+    messages: {
+        "en": {
+            "global": {
+                "error": {
+                    "500": "Internal Server Error."
                 }
-            },
-            "zh-CN": {
-                "global": {
-                    "error": {
-                        "500": "服务器异常，请稍后再试！"
-                    }
+            }
+        },
+        "zh-CN": {
+            "global": {
+                "error": {
+                    "500": "服务器异常，请稍后再试！"
                 }
             }
         }
-    })
+    }
+});
+new Vue({
+    i18n,
+    router,
+    data: {
+        locale: lang
+    },
+    watch: {
+        locale(lng) {
+            i18n.locale = lng;
+            this.$cookie.set("locale", lng, { expires: "1Y" });
+        }
+    }
 }).$mount("#app");
